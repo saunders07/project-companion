3339b1ef88b42cb81e28a9c4348b4f6b7642f126
Ben Saunders <b.saunders1996@icloud.com>
2018-02-25 12:03:18 +0000

    changes to make the homepage accomodate the announcements

---
+++ getAnnouncementsFrontPage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+
+    <?php
+    $q = intval($_GET['q']);
+
+    $con = mysqli_connect('localhost','saunders','','testAJAX');
+    if (!$con) {
+        die('Could not connect: ' . mysqli_error($con));
+    }
+    $sql="SELECT * FROM test_announcements";
+    $result = mysqli_query($con,$sql);
+    while($row = mysqli_fetch_array($result)) {
+            echo "<div class='col-md-12 announcementsFrontPage'>";
+            echo "<p><span class='nd2-title'>" . $row['ann_date'] . " - " . $row['ann_title'] . "</span><br>";
+            echo "<span class=''>" . $row['ann_content'] . "</span></br>";
+            echo "</br>";
+            echo "<span class=''> Posted By: " . $row['tutor'] . "</span></p>";
+            echo "</div>";
+    }
+    echo "</table>";
+    mysqli_close($con);
+    ?>
+    </body>
+</html>
