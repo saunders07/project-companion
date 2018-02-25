4a4bf7b431c66e7b626777952af09c8bef6ac4f3
Ben Saunders <b.saunders1996@icloud.com>
2018-02-25 21:06:17 +0000

    chnages to the login system for email verification

---
+++ login/index.js
@@ -0,0 +1,82 @@
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+  
+      document.getElementById("user_div").style.display = "block";
+      document.getElementById("login_div").style.display = "none";
+  
+      var user = firebase.auth().currentUser;
+      var email_verified = user.emailVerified;
+      console.log(email_verified);
+
+      if(user != null){
+        if (email_verified) {
+            document.getElementById("user_para").innerHTML = "Lets do this!"
+            document.getElementById("verify_btn").style.display = "none";
+        } else {
+            document.getElementById("user_para").innerHTML = "Please verify email to continue!"
+            document.getElementById("user_para").innerHTML += "<br>Verified : " + email_verified;
+            document.getElementById("verify_btn").style.display = "block";
+        }  
+      }
+  
+    } else {
+      // No user is signed in.
+  
+      document.getElementById("user_div").style.display = "none";
+      document.getElementById("login_div").style.display = "block";
+  
+    }
+  });
+  
+  function login(){
+  
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+  
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+  
+      window.alert("Error : " + errorMessage);
+  
+      // ...
+    });
+  
+  }
+
+  function create_account() {
+
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+
+    firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+    
+        window.alert("Error : " + errorMessage);
+
+        // send_verification();
+        // ...
+      });
+
+  }
+
+  function send_verification () {
+
+    var user = firebase.auth().currentUser;
+    
+    user.sendEmailVerification().then(function() {
+
+        window.alert("Verification sent!")
+    }).catch(function(error) {
+        window.alert("Error : " + error.message)
+    });
+  }
+  
+  function logout(){
+    firebase.auth().signOut();
+  }
+  
