58d07efb6906507fee706d523cd8cf270d79b237
Ben Saunders <b.saunders1996@icloud.com>
2018-02-27 10:10:16 +0000

    chnages to the announcmeents and segregating code

--- calendar.js
+++ calendar.js
@@ -1,7 +1,113 @@
 var d = new Date();
 var n = d.getDay();
 var j = "";
+var l = "";
 
+// GET DATA FOR HNDY2
+switch (n) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        console.log(day);
+        $.getJSON("HNDY2calendar.json", function(data){
+            var calendar = data.calendar.monday;
+            for (q in calendar) {
+                l += "<div class='calendar' id =" + [q] + "><p class ='nd2-title assignmentNameAndNumber'>" + calendar[q].timeframe + "</p>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+                l += "<span class='calendarInfo'> Unit Name: " + calendar[q].unit_number + "</span><br>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+                l += "<span class='calendarInfo'> Unit Number: " + calendar[q].unit_name + "</span><br>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+                l += "<span class='calendarInfo'> Lecturer: " + calendar[q].lecturer + "</span><br>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+                l += "<span class='calendarInfo'> Room: " + calendar[q].room + "</span></div>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+            }
+        });
+        break;
+    case 2:
+        var day = "Tuesday";
+        $.getJSON("HNDY2calendar.json", function(data){
+            var calendar = data.calendar.tuesday;
+            console.log(document.getElementById("dayTitle"));
+            for (q in calendar) {
+                l += "<div class='calendar' id =" + [q] + "><p class ='nd2-subhead assignmentNameAndNumber'>" + calendar[q].timeframe + "</p>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+                l += "<span class='calendarInfo'> Unit Name: " + calendar[q].unit_number + "</span><br>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+                l += "<span class='calendarInfo'> Unit Number: " + calendar[q].unit_name + "</span><br>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+                l += "<span class='calendarInfo'> Lecturer: " + calendar[q].lecturer + "</span><br>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+                l += "<span class='calendarInfo'> Room: " + calendar[q].room + "</span></div>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+            }
+        });
+        break;
+    case 3:
+        day = "Wednesday";
+        $.getJSON("HNDY2calendar.json", function(data){
+            var calendar = data.calendar.wednesday;
+            console.log(document.getElementById("dayTitle"));
+            for (q in calendar) {
+                l += "<div class='calendar' id =" + [q] + "><p class ='nd2-subhead assignmentNameAndNumber'>" + calendar[q].timeframe + "</p>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+                l += "<span class='calendarInfo'> Unit Name: " + calendar[q].unit_number + "</span><br>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+                l += "<span class='calendarInfo'> Unit Number: " + calendar[q].unit_name + "</span><br>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+                l += "<span class='calendarInfo'> Lecturer: " + calendar[q].lecturer + "</span><br>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+                l += "<span class='calendarInfo'> Room: " + calendar[q].room + "</span></div>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+            }
+        });
+        break;
+    case 4:
+        day = "Thursday";
+        $.getJSON("HNDY2calendar.json", function(data){
+            var calendar = data.calendar.thursday;
+            console.log(document.getElementById("dayTitle"));
+            for (q in calendar) {
+                l += "<div class='calendar' id =" + [q] + "><p class ='nd2-subhead assignmentNameAndNumber'>" + calendar[q].timeframe + "</p>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+                l += "<span class='calendarInfo'> Unit Name: " + calendar[q].unit_number + "</span><br>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+                l += "<span class='calendarInfo'> Unit Number: " + calendar[q].unit_name + "</span><br>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+                l += "<span class='calendarInfo'> Lecturer: " + calendar[q].lecturer + "</span><br>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+                l += "<span class='calendarInfo'> Room: " + calendar[q].room + "</span></div>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+            }
+        });
+        break;
+    case 5:
+        day = "Friday";
+        $.getJSON("HNDY2calendar.json", function(data){
+            var calendar = data.calendar.friday;
+            console.log(document.getElementById("dayTitle"));
+            for (q in calendar) {
+                l += "<div class='calendar' id =" + [q] + "><p class ='nd2-subhead assignmentNameAndNumber'>" + calendar[q].timeframe + "</p>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+                l += "<span class='calendarInfo'> Unit Name: " + calendar[q].unit_number + "</span><br>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+                l += "<span class='calendarInfo'> Unit Number: " + calendar[q].unit_name + "</span><br>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+                l += "<span class='calendarInfo'> Lecturer: " + calendar[q].lecturer + "</span><br>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+                l += "<span class='calendarInfo'> Room: " + calendar[q].room + "</span></div>";
+                    document.getElementById("calendarHNDY2").innerHTML = l;
+            }
+        });
+        break;
+    case 6:
+        day = "Saturday";
+}
+
+// GET DATA FOR BSC HONS
 switch (n) {
     case 0:
         day = "Sunday";
@@ -10,7 +116,7 @@ switch (n) {
         day = "Monday";
         console.log(day);
         $.getJSON("BSCHONScalendar.json", function(data){
-            var calendar = data.calendar;
+            var calendar = data.calendar.monday;
             for (q in calendar) {
                 j += "<div class='calendar' id =" + [q] + "><p class ='nd2-title assignmentNameAndNumber'>" + calendar[q].timeframe + "</p>";
                     document.getElementById("calendarBSCHONS").innerHTML = j;
@@ -26,16 +132,80 @@ switch (n) {
         });
         break;
     case 2:
-        day = "Tuesday";
+        var day = "Tuesday";
+        $.getJSON("BSCHONScalendar.json", function(data){
+            var calendar = data.calendar.tuesday;
+            console.log(document.getElementById("dayTitle"));
+            for (q in calendar) {
+                j += "<div class='calendar' id =" + [q] + "><p class ='nd2-subhead assignmentNameAndNumber'>" + calendar[q].timeframe + "</p>";
+                    document.getElementById("calendarBSCHONS").innerHTML = j;
+                j += "<span class='calendarInfo'> Unit Name: " + calendar[q].unit_number + "</span><br>";
+                    document.getElementById("calendarBSCHONS").innerHTML = j;
+                j += "<span class='calendarInfo'> Unit Number: " + calendar[q].unit_name + "</span><br>";
+                    document.getElementById("calendarBSCHONS").innerHTML = j;
+                j += "<span class='calendarInfo'> Lecturer: " + calendar[q].lecturer + "</span><br>";
+                    document.getElementById("calendarBSCHONS").innerHTML = j;
+                j += "<span class='calendarInfo'> Room: " + calendar[q].room + "</span></div>";
+                    document.getElementById("calendarBSCHONS").innerHTML = j;
+            }
+        });
         break;
     case 3:
         day = "Wednesday";
+        $.getJSON("BSCHONScalendar.json", function(data){
+            var calendar = data.calendar.wednesday;
+            console.log(document.getElementById("dayTitle"));
+            for (q in calendar) {
+                j += "<div class='calendar' id =" + [q] + "><p class ='nd2-subhead assignmentNameAndNumber'>" + calendar[q].timeframe + "</p>";
+                    document.getElementById("calendarBSCHONS").innerHTML = j;
+                j += "<span class='calendarInfo'> Unit Name: " + calendar[q].unit_number + "</span><br>";
+                    document.getElementById("calendarBSCHONS").innerHTML = j;
+                j += "<span class='calendarInfo'> Unit Number: " + calendar[q].unit_name + "</span><br>";
+                    document.getElementById("calendarBSCHONS").innerHTML = j;
+                j += "<span class='calendarInfo'> Lecturer: " + calendar[q].lecturer + "</span><br>";
+                    document.getElementById("calendarBSCHONS").innerHTML = j;
+                j += "<span class='calendarInfo'> Room: " + calendar[q].room + "</span></div>";
+                    document.getElementById("calendarBSCHONS").innerHTML = j;
+            }
+        });
         break;
     case 4:
         day = "Thursday";
+        $.getJSON("BSCHONScalendar.json", function(data){
+            var calendar = data.calendar.thursday;
+            console.log(document.getElementById("dayTitle"));
+            for (q in calendar) {
+                j += "<div class='calendar' id =" + [q] + "><p class ='nd2-subhead assignmentNameAndNumber'>" + calendar[q].timeframe + "</p>";
+                    document.getElementById("calendarBSCHONS").innerHTML = j;
+                j += "<span class='calendarInfo'> Unit Name: " + calendar[q].unit_number + "</span><br>";
+                    document.getElementById("calendarBSCHONS").innerHTML = j;
+                j += "<span class='calendarInfo'> Unit Number: " + calendar[q].unit_name + "</span><br>";
+                    document.getElementById("calendarBSCHONS").innerHTML = j;
+                j += "<span class='calendarInfo'> Lecturer: " + calendar[q].lecturer + "</span><br>";
+                    document.getElementById("calendarBSCHONS").innerHTML = j;
+                j += "<span class='calendarInfo'> Room: " + calendar[q].room + "</span></div>";
+                    document.getElementById("calendarBSCHONS").innerHTML = j;
+            }
+        });
         break;
     case 5:
         day = "Friday";
+        $.getJSON("BSCHONScalendar.json", function(data){
+            var calendar = data.calendar.fridday;
+            console.log(document.getElementById("dayTitle"));
+            for (q in calendar) {
+                j += "<div class='calendar' id =" + [q] + "><p class ='nd2-subhead assignmentNameAndNumber'>" + calendar[q].timeframe + "</p>";
+                    document.getElementById("calendarBSCHONS").innerHTML = j;
+                j += "<span class='calendarInfo'> Unit Name: " + calendar[q].unit_number + "</span><br>";
+                    document.getElementById("calendarBSCHONS").innerHTML = j;
+                j += "<span class='calendarInfo'> Unit Number: " + calendar[q].unit_name + "</span><br>";
+                    document.getElementById("calendarBSCHONS").innerHTML = j;
+                j += "<span class='calendarInfo'> Lecturer: " + calendar[q].lecturer + "</span><br>";
+                    document.getElementById("calendarBSCHONS").innerHTML = j;
+                j += "<span class='calendarInfo'> Room: " + calendar[q].room + "</span></div>";
+                    document.getElementById("calendarBSCHONS").innerHTML = j;
+            }
+        });
         break;
     case 6:
         day = "Saturday";
---
+++ getAssignments.js
@@ -0,0 +1,56 @@
+// GET JSON FOR HNDY1 ASSIGNMENTS
+var e = "";
+
+	$.getJSON("HNDY1Assignments.json", function(data){
+	    var assignments = data.assignments;
+		for (q in assignments) {
+			e += "<div class='assignments' id =" + [q] + "><p class ='nd2-title assignmentNameAndNumber'>" + assignments[q].unit_number + " " + assignments[q].unit_name + "</p>";
+				document.getElementById("assignmentsListHNDY1").innerHTML = e;
+		    e += "<span class='assignmentInfo'> Unit Name: " + assignments[q].unit_name + "</span></br>";
+				document.getElementById("assignmentsListHNDY1").innerHTML = e;
+			e += "<span class='assignmentInfo'>Assignment Description: " + assignments[q].assign_desc + "</span></br>";
+				document.getElementById("assignmentsListHNDY1").innerHTML = e;
+			e += "<span class='assignmentInfo'>Hand Out:<span class='handOut'> " + assignments[q].handoutdate + "</span></span><br>";
+				document.getElementById("assignmentsListHNDY1").innerHTML = e;
+			e += "<span class='assignmentInfo'>Hand In:<span class='handIn'> " + assignments[q].handindate + "</span></span></div>";
+				document.getElementById("assignmentsListHNDY1").innerHTML = e;
+		}
+        });
+
+// GET JSON FOR HNDY2 ASSIGNMENTS
+var j = "";
+
+    $.getJSON("HNDY2Assignments.json", function(data){
+        var assignments = data.assignments;
+        for (q in assignments) {
+            j += "<div class='assignments' id =" + [q] + "><p class ='nd2-title assignmentNameAndNumber'>" + assignments[q].unit_number + " " + assignments[q].unit_name + "</p>";
+                document.getElementById("assignmentsListHNDY2").innerHTML = j;
+            j += "<span class='assignmentInfo'> Unit Name: " + assignments[q].unit_name + "</span><br>";
+                document.getElementById("assignmentsListHNDY2").innerHTML = j;
+            j += "<span class='assignmentInfo'>Assignment Description: " + assignments[q].assign_desc + "</span><br>";
+                document.getElementById("assignmentsListHNDY2").innerHTML = j;
+            j += "<span class='assignmentInfo'>Hand Out:<span class='handOut'> " + assignments[q].handoutdate + "</span></span><br>";
+                document.getElementById("assignmentsListHNDY2").innerHTML = j;
+            j += "<span class='assignmentInfo'>Hand In:<span class='handIn'> " + assignments[q].handindate + "</span></span></div>";
+                document.getElementById("assignmentsListHNDY2").innerHTML = j;
+        }
+        });
+
+// GET JSON FOR BSCHONS ASSIGNMENTS
+var h = "";
+
+    $.getJSON("BSCHONSAssignments.json", function(data){
+        var assignments = data.assignments;
+        for (q in assignments) {
+            h += "<div class='assignments' id =" + [q] + "><p class ='nd2-title assignmentNameAndNumber'>" + assignments[q].unit_number + " " + assignments[q].unit_name + "</p>";
+                document.getElementById("assignmentsListBSCHONS").innerHTML = h;
+            h += "<span class='assignmentInfo'> Unit Name: " + assignments[q].unit_name + "</span><br>";
+                document.getElementById("assignmentsListBSCHONS").innerHTML = h;
+            h += "<span class='assignmentInfo'>Assignment Description: " + assignments[q].assign_desc + "</span><br>";
+                document.getElementById("assignmentsListBSCHONS").innerHTML = h;
+            h += "<span class='assignmentInfo'>Hand Out:<span class='handOut'> " + assignments[q].handoutdate + "</span></span><br>";
+                document.getElementById("assignmentsListBSCHONS").innerHTML = h;
+            h += "<span class='assignmentInfo'>Hand In:<span class='handIn'> " + assignments[q].handindate + "</span></span></div>";
+                document.getElementById("assignmentsListBSCHONS").innerHTML = h;
+        }
+        });
