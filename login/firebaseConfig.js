fd91a492202bc224840f1b6838b92e6f7667e696
Ben Saunders <b.saunders1996@icloud.com>
2018-02-27 11:04:32 +0000

    added config for login

---
+++ login/firebaseConfig.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyAX_PUNwLjUhEa3-E81HEd7DdobNV802VU",
+    authDomain: "test-ba798.firebaseapp.com",
+    databaseURL: "https://test-ba798.firebaseio.com",
+    projectId: "test-ba798",
+    storageBucket: "test-ba798.appspot.com",
+    messagingSenderId: "694929286079"
+};
+firebase.initializeApp(config);
